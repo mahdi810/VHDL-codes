8af36570cb852f4e5732bd52d7b29cb21f68648f
Mohammad Mahdi Mohammadi <mmohammadmahdi496@gmail.com>
2026-03-23 09:20:31 +0100

    clk division done 2

---
+++ using_vivado/clkdiv/clkdiv.srcs/sim_1/new/clkdiv_tb.vhd
@@ -0,0 +1,66 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/23/2026 08:57:17 AM
+-- Design Name: 
+-- Module Name: clkdiv_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all; 
+
+entity clkdiv_tb is
+end clkdiv_tb;
+
+architecture Behavioral of clkdiv_tb is
+    component clkdiv is 
+        Port (clk : in std_logic; 
+        reset: in std_logic; 
+        clk_out : out std_logic );
+    end component clkdiv; 
+    signal clk, clk_out, reset : std_logic := '0'; 
+    constant clk_period : time := 10 ns; 
+begin
+    
+    --clock generation 
+    clk_p : process 
+    begin 
+        clk <= '0'; 
+        wait for clk_period/2; 
+        clk <= '1'; 
+        wait for clk_period/2; 
+    end process clk_p; 
+    
+    --unit under test 
+    uut : clkdiv 
+    port map(clk => clk, 
+             clk_out => clk_out, 
+             reset => reset ); 
+             
+             
+    stim_p : process
+    begin 
+        reset <= '1'; 
+        wait for clk_period*4; 
+        reset <= '0';
+        wait for clk_period*20;  
+    
+    
+    wait; 
+    end process stim_p;
+    
+end Behavioral;
---
+++ using_vivado/clkdiv/clkdiv.srcs/sources_1/new/clkdiv.vhd
@@ -0,0 +1,56 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/23/2026 08:56:59 AM
+-- Design Name: 
+-- Module Name: clkdiv - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all; 
+
+entity clkdiv is
+  Port (clk : in std_logic; 
+        reset: in std_logic; 
+        clk_out : out std_logic );
+end clkdiv;
+
+architecture Behavioral of clkdiv is
+signal clk_outi : std_logic := '0'; 
+constant max_count : integer := 4; 
+
+begin
+    clk_div : process(clk, reset)
+    variable cnt: integer range 0 to max_count; 
+    begin 
+        if rising_edge(clk) then 
+            if reset = '1' then 
+                cnt := 0; 
+                clk_outi <= '0'; 
+            else 
+                if cnt = 0 then 
+                    clk_outi <= NOT clk_outi; 
+                    cnt := max_count; 
+                else 
+                    cnt := cnt - 1; 
+                end if; 
+            end if; 
+        end if; 
+    end process clk_div; 
+    clk_out <= clk_outi; 
+
+end Behavioral;
